027a618c8d309d1f01d0dba8fdbfd2a659097d9a
Omurbek <firefoxer00@gmail.com>
2024-03-06 18:05:08 +0600

    first commit

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<script setup>
+defineProps({
+  imageUrl: String,
+  title: String,
+  price: Number,
+  isFavorite: Boolean,
+  isAdded: Boolean
+})
+</script>
+
+<template>
+  <div
+    class="relative m-10 border border-slate-100 rounded-2xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Like 1"
+      class="absolute top-0 left-0"
+    />
+    <img :src="imageUrl" alt="Sneaker" />
+    <p class="mt-2">{{ title }}</p>
+
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <span>{{ price }} руб.</span>
+      </div>
+
+      <img :src="!isAdded ? '/plus.svg' : '/checked.svg'" alt="Plus" />
+    </div>
+  </div>
+</template>
---
+++ src/components/CardList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      image-url="/sneakers/sneakers-1.jpg"
+      title="Мужские кроссовки Nike Blazzer Mid Suede"
+      :price="1200"
+    />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
